5735a78d90b18ca2deea1c516ce2c98e2adac80d
sanjana <sanjana.shanigaram@gmail.com>
2024-01-11 10:08:01 -0600

    Student commit

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public static void main(String args[])
+    {
+        Student s1= new Student();
+       System.out.println(s1.getName());
+    }
+   public String getName()
+   {
+      String name="Sanjana";
+       return name;
+   }
+}
