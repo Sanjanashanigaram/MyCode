50736c8aaab8719015c74fb6e0dbec4f866ccedb
sanjana <sanjana.shanigaram@gmail.com>
2024-02-12 13:42:08 -0600

    commit

---
+++ src/test/java/threadsTest/SingletonTest.java
@@ -0,0 +1,24 @@
+package threadsTest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import threads.Singleton;
+
+public class SingletonTest extends TestCase {
+    public void testSingletonInstance() {
+        // Get the singleton instance
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        // Check if both instances are the same object
+        assertEquals("Both instances should be the same object", instance1, instance2);
+    }
+
+    public void testSingletonOperation() {
+        // Get the singleton instance
+        Singleton instance = Singleton.getInstance();
+        // Perform an operation
+        instance.performOperation();
+        // Add more assertions if needed
+    }
+}
